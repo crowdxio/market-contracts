bfdd45ba124424cab8cf3ad13ea7e3f2544481e1
Mihai Cimpoesu <mihai.cimpoesu@135b.io>
2018-05-30 12:03:32 +0100

    Second iteration through the UniqxMarketAdapt

--- contracts/UniqxMarketAdapt.sol
+++ contracts/UniqxMarketAdapt.sol
@@ -5,6 +5,7 @@ import "../zeppelin/contracts/ownership/NoOwner.sol";
 import "../zeppelin/contracts/lifecycle/Pausable.sol";
 import "../zeppelin/contracts/math/SafeMath.sol";
 import "../zeppelin/contracts/token/ERC721/ERC721BasicToken.sol";
+import {Collectibles} from "../adapt/contracts/Collectibles.sol";
 
 contract UniqxMarketAdapt is NoOwner, Pausable {
 
@@ -12,7 +13,7 @@ contract UniqxMarketAdapt is NoOwner, Pausable {
 
 	address public MARKET_ADMIN_MSIG;
 	address public MARKET_FEES_MSIG;
-	ERC721BasicToken public AdaptToken;
+	Collectibles public AdaptToken;
 
 	uint public RESERVATION_TIME = 3 days;
 
@@ -24,6 +25,7 @@ contract UniqxMarketAdapt is NoOwner, Pausable {
 	enum TokenStatus {
 		Unknown,
 		Published,
+		Cancelled,
 		Reserved,
 		Acquired
 	}
@@ -33,6 +35,7 @@ contract UniqxMarketAdapt is NoOwner, Pausable {
 		uint publishTime;
 		uint acquirePrice;
 		uint acquireTime;
+		TokenStatus status;
 		address seller;
 	}
 
@@ -47,7 +50,7 @@ contract UniqxMarketAdapt is NoOwner, Pausable {
 
 		MARKET_ADMIN_MSIG = _marketAdmin;
 		MARKET_FEES_MSIG = _marketFees;
-		AdaptToken = ERC721BasicToken(_adaptContract);
+		AdaptToken = Collectibles(_adaptContract);
 
 		transferOwnership(_marketAdmin);
 	}
@@ -55,22 +58,7 @@ contract UniqxMarketAdapt is NoOwner, Pausable {
 	function getTokenStatus(uint _tokenId) public view
 		returns (TokenStatus _status) {
 
-		NftToken storage nftToken = tokens[_tokenId];
-
-		if(nftToken.publishTime == 0) {
-			return TokenStatus.Unknown;
-		}
-
-		if(nftToken.acquirePrice == 0) {
-			if(reservations[_tokenId] != address(0x0) &&
-				nftToken.publishTime + RESERVATION_TIME > now) {
-				return TokenStatus.Reserved;
-			} else {
-				return TokenStatus.Published;
-			}
-		}
-
-		return TokenStatus.Acquired;
+		return tokens[_tokenId].status;
 	}
 
 	function getTokenInfo(uint _tokenId) public view
@@ -83,13 +71,12 @@ contract UniqxMarketAdapt is NoOwner, Pausable {
 			address _seller
 		) {
 
-		_status = getTokenStatus(_tokenId);
-
 		NftToken storage nftToken = tokens[_tokenId];
 		_publishPrice = nftToken.publishPrice;
 		_publishTime = nftToken.publishTime;
 		_acquirePrice = nftToken.acquirePrice;
 		_acquireTime = nftToken.acquireTime;
+		_status = nftToken.status;
 		_seller = nftToken.seller;
 	}
 
@@ -117,8 +104,13 @@ contract UniqxMarketAdapt is NoOwner, Pausable {
 		require(_tokenIds.length == _reservations.length);
 
 		for(uint index=0; index<_tokenIds.length; index++) {
-			// this token is not already published
-			require(tokens[_tokenIds[index]].publishTime == 0);
+			// token must not be published on the market
+			NftToken storage existingToken = tokens[_tokenIds[index]];
+			require(
+				existingToken.status == TokenStatus.Unknown ||
+				existingToken.status == TokenStatus.Cancelled
+			);
+
 			// make sure the seller is approved to publish this item
 			require(isSpenderApproved(msg.sender, _tokenIds[index]));
 
@@ -131,13 +123,36 @@ contract UniqxMarketAdapt is NoOwner, Pausable {
 					publishTime: now,
 					acquirePrice: 0,
 					acquireTime: 0,
+					status: TokenStatus.Published,
 					seller: msg.sender
 				});
-			tokens[_tokenIds[index]] = nftToken;
 
 			if(_reservations[index] != address(0x0)) {
 				reservations[_tokenIds[index]] = _reservations[index];
+				nftToken.status = TokenStatus.Reserved;
 			}
+
+			tokens[_tokenIds[index]] = nftToken;
+		}
+	}
+
+	function cancel(uint [] _tokenIds) public whenNotPaused {
+
+		for(uint index=0; index<_tokenIds.length; index++) {
+			NftToken storage nftToken = tokens[_tokenIds[index]];
+			// only the original seller can cancel
+			require(msg.sender == nftToken.seller);
+
+			// token must still be published on the market
+			require(
+				nftToken.status == TokenStatus.Published ||
+				nftToken.status == TokenStatus.Reserved
+			);
+			// token must still be in temporary custody of the market
+			require(AdaptToken.ownerOf(_tokenIds[index]) == address(this));
+
+			AdaptToken.transferFrom(address(this), nftToken.seller, _tokenIds[index]);
+			nftToken.status = TokenStatus.Cancelled;
 		}
 	}
 
@@ -145,12 +160,17 @@ contract UniqxMarketAdapt is NoOwner, Pausable {
 		public payable whenNotPaused {
 
 		NftToken storage nftToken = tokens[_tokenId];
-		require(nftToken.publishPrice > 0);
-		require(nftToken.acquirePrice == 0);
-		require(msg.value >= nftToken.publishPrice);
-		require(isSpenderApproved(msg.sender, _tokenId));
 
-		if(reservations[_tokenId] != address(0x0) &&
+		// token must still be published on the market
+		require(
+			nftToken.status == TokenStatus.Published ||
+			nftToken.status == TokenStatus.Reserved
+		);
+		// the amount of ETH forwarded is higher than publish price
+		require(msg.value >= nftToken.publishPrice);
+
+		// the token must be reserved for the current buyer
+		if(nftToken.status == TokenStatus.Reserved &&
 			nftToken.publishTime + RESERVATION_TIME > now ) {
 			require(msg.sender == reservations[_tokenId]);
 		}
@@ -158,7 +178,10 @@ contract UniqxMarketAdapt is NoOwner, Pausable {
 		uint marketFee = msg.value.mul(MARKET_FEE_NUM).div(MARKET_FEE_DEN);
 		uint sellerDue = msg.value.sub(marketFee);
 
+		nftToken.status = TokenStatus.Acquired;
+		AdaptToken.setTokenMetadata(_tokenId, now, msg.value);
 		AdaptToken.transferFrom(address(this), msg.sender, _tokenId);
+
 		MARKET_FEES_MSIG.transfer(marketFee);
 		nftToken.seller.transfer(sellerDue);
 	}
